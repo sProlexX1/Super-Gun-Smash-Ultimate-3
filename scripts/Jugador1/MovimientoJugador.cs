e1bb269070997c56fa0b379ad769ef9bfbd606f7
sProlexX1 <d.cancino8@alumnos.santotomas.cl>
2023-05-25 15:37:04 -0400

    Se agrega disparar a ambos lados y traspasar plataforma

--- scripts/Jugador1/MovimientoJugador.cs
+++ scripts/Jugador1/MovimientoJugador.cs
@@ -19,6 +19,8 @@ public class MovimientoJugador : MonoBehaviour
 
     private bool mirandoDerecha = true;
 
+    public Vector2 input;
+
     //SALTO
 
     [SerializeField] private float fuerzaSalto;
@@ -26,6 +28,9 @@ public class MovimientoJugador : MonoBehaviour
     [SerializeField] private Transform controladorSuelo;
     [SerializeField] private Vector3 dimensionesCaja;
     [SerializeField] private bool enSuelo;
+    [SerializeField] private bool enFuerza;
+
+
     private bool salto = false;
 
 
@@ -39,12 +44,16 @@ public class MovimientoJugador : MonoBehaviour
 
     private void Update()
     {
+        input.x = Input.GetAxisRaw("HorizontalJugador1");
+        input.y = Input.GetAxisRaw("VerticalJugador1");
 
-
-        movimientoHorizontal = Input.GetAxisRaw("HorizontalJugador1") * velocidadDeMovimiento;
+        //movimientoHorizontal = Input.GetAxisRaw("HorizontalJugador1") * velocidadDeMovimiento;
+        movimientoHorizontal = input.x * velocidadDeMovimiento;
 
         animator.SetFloat("Horizontal1", Mathf.Abs(movimientoHorizontal));
 
+
+        /*
         if (movimientoHorizontal < 0.0f)//GIRAR DERECHA
         {
             transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
@@ -53,12 +62,22 @@ public class MovimientoJugador : MonoBehaviour
         {
             transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         }
+        */
 
         if (Input.GetKeyDown(KeyCode.W))//SALTAR
         {
-            salto = true;
+            if (input.y >= 0)
+            {
+                salto = true;
+            }else
+            {
+                DesactivarPlataformas();
+            }
+            
         }
     }
+
+    //HOLAaa
 
     private void FixedUpdate()
     {
@@ -71,10 +90,34 @@ public class MovimientoJugador : MonoBehaviour
         salto = false;
     }
 
+    private void DesactivarPlataformas()
+    {
+        Collider2D[] objetos = Physics2D.OverlapBoxAll(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
+        foreach(Collider2D item in objetos)
+        {
+            PlatformEffector2D platformEffector2D =  item.GetComponent<PlatformEffector2D>();
+            if(platformEffector2D != null)
+            {
+                //Physics2D para ignorar  colisiones, esta  toma 2 colliders,el primero es del  jugador y el segundo es
+                //el objeto que tocamos. por ultimo el true es para que desactive la colision.
+                Physics2D.IgnoreCollision(GetComponent<Collider2D>(), item.GetComponent<Collider2D>(), true);
+            }
+        }
+    }
+
     private void Mover(float mover, bool saltar)
     {
         Vector3 velocidadObjetivo = new Vector2(mover, rgbd2D.velocity.y);
         rgbd2D.velocity = Vector3.SmoothDamp(rgbd2D.velocity, velocidadObjetivo, ref velocidad, suavizadoDeMovimiento);
+
+        if(mover > 0 && !mirandoDerecha)
+        {
+            Girar();
+        }
+        else if(mover < 0 && mirandoDerecha)
+        {
+            Girar();
+        }
 
 
         if(enSuelo && saltar)
@@ -82,6 +125,12 @@ public class MovimientoJugador : MonoBehaviour
             enSuelo = false;
             rgbd2D.AddForce(new Vector2(0f, fuerzaSalto));
         }
+    }
+
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
     }
 
 
